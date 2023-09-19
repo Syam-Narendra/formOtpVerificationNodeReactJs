01b1d689864bd8b34813d70c6ad8a03a65bad536
Syam Narendra <syamnarendra85@gmail.com>
2023-09-19 21:24:51 +0530

    OTP Success page and Otp backend sent function added

--- server.js
+++ server.js
@@ -7,13 +7,55 @@ const corsOptions ={
    credentials:true,
    optionSuccessStatus:200,
 }
-
 app.use(cors(corsOptions))
 
+const accountSid = 'ACdb952fdd6a8b53a1c39b007b10f71d46';
+const authToken = '8d25fd50a101778ad860b6bc3b77ac38';
+const client = require('twilio')(accountSid, authToken);
+
+let otpNumber;
 app.get('/', (req, res) => {
     res.send(JSON.stringify("hello world"))
 })
 
+app.get('/send-otp', async(req, res) => {
+    try{
+        const generatedOtp = Math.floor(Math.random() * 100000);
+        otpNumber = generatedOtp
+        const bodyParams = {body: 'Your One Time Password is '+ generatedOtp, from: '+12566459736', to: '+919494909811'}
+        const message = await client.messages.create(bodyParams)
+        res.send(message)
+    }catch(err){
+        res.send(JSON.stringify(err.status));
+    }
+
+});
+
+app.get('/verify-otp',(req, res) => {
+    
+});
+
 app.listen(4000,() => {
     console.log("listening on port 4000")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,32 +3,83 @@ import { Component } from "react";
 
 class Home extends Component{
 
-    getData=event=>{
+    state = {fName:"",lName:"",mobileNumber:"", otpSent:null}
+
+    onchangeFname=event=>{
+      this.setState({fName:event.target.value});
+    }
+
+    onchangeLname=event=>{
+      this.setState({lName:event.target.value});
+    }
+
+    onChangeMobileNumber=event=>{
+      this.setState({mobileNumber:event.target.value});
+    }
+
+    getData=async event=>{
         event.preventDefault();
         console.log("getData");
+        const response = await fetch('http://localhost:4000/send-otp');
+        const data = await response.json();
+        console.log(data.status==="queued");
+        if(data.status==="queued"){
+          this.setState({otpSent: true});
+        }else{
+          this.setState({otpSent: false});
+        }
     }
-    render(){
-        return (
-            <form onSubmit={this.getData} className="form">
+
+
+    renderHome=()=>{
+      const {otpSent} = this.state
+      return(
+      <form onSubmit={this.getData} className="form">
             <div className="title">Welcome</div>
             <div className="subtitle">Let's create your account!</div>
             <div className="input-container ic1">
-              <input id="firstname" className="input" type="text" placeholder=" " />
+              <input onChange={this.onchangeFname} id="firstname" className="input" type="text" placeholder=" " />
               <div className="cut"></div>
               <label htmlFor="firstname" className="placeholder">First name</label>
             </div>
             <div className="input-container ic2">
-              <input id="lastname" className="input" type="text" placeholder=" " />
+              <input onChange={this.onchangeLname} id="lastname" className="input" type="text" placeholder=" " />
               <div className="cut"></div>
               <label htmlFor="lastname" className="placeholder">Last name</label>
             </div>
             <div className="input-container ic2">
-              <input id="email" className="input" type="text" placeholder=" " />
+              <input onChange={this.onChangeMobileNumber} id="email" className="input" type="number" placeholder=" " />
               <div className="cut cut-short"></div>
-              <label htmlFor="email" className="placeholder">Email</label>
+              <label htmlFor="email" className="placeholder">Mobile</label>
+            </div>
+            <button type="submit" className="submit">Submit</button>
+            { otpSent===false && <p className="errorMsg">Something Went Wrong</p> }
+          </form>
+      )
+
+    }
+
+    renderOtpSentPage=()=>{
+      return(
+        <div className="form">
+          <h1 className="title">OTP Sent Successfully</h1>
+          <form>
+          <div className="input-container ic2">
+              <input min={5} id="otp-input" className="input" type="number" placeholder=" " />
+              <div className="cut"></div>
+              <label htmlFor="otp-input" className="placeholder">Enter OTP</label>
             </div>
             <button type="submit" className="submit">Submit</button>
           </form>
+        </div>
+      )
+    }
+    render(){
+        const {otpSent}= this.state
+        return (
+            <div>
+              {otpSent ? this.renderOtpSentPage(): this.renderHome()}
+            </div>
         )
     }
 } 
